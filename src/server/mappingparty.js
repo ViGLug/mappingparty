b5b713b10b87bd8730cd5aa2e59da6ec6f1d4b15
Francesco Frassinelli <fraph24@gmail.com>
2014-04-05 16:36:20 +0200

    Collection users is reserved (2)

--- src/server/mappingparty.js
+++ src/server/mappingparty.js
@@ -1,10 +1,17 @@
-Users = new Meteor.Collection('users');
+// Users = new Meteor.Collection('users');
 Tags = new Meteor.Collection('tags');
 Elements = new Meteor.Collection('elements');
 
 Meteor.startup(function () {
+    // Cleanup
+//     Users.remove({});
+    Tags.remove({});
+    Elements.remove({});
+    
     if (Tags.find().count() === 0) {
-        Tags.insert({name: "Cestino", osm_key:'amenity',
+        Tags.insert({name:"Cestino", osm_key:'amenity',
             osm_value:['waste_disposal']});
+        Tags.insert({name:"Autonoleggio", osm_key:'amenity',
+            osm_value:['car_rental']});
     }
 });
