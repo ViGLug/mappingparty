86edf151ad3721d56aa85b22cf7059ce29ad50f4
Francesco Frassinelli <fraph24@gmail.com>
2014-04-05 15:25:49 +0200

    Collections correctly exposed

--- src/client/mappingparty.js
+++ src/client/mappingparty.js
@@ -2,3 +2,6 @@ Meteor.subscribe('users');
 Meteor.subscribe('tags');
 Meteor.subscribe('elements');
 
+Users = new Meteror.Collection('users');
+Tags = new Meteror.Collection('tags');
+Elements = new Meteror.Collection('elements');
