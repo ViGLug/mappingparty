8b31d413310ee7885089b5b34741b11c34d85d7e
Francesco Frassinelli <fraph24@gmail.com>
2014-04-05 15:26:28 +0200

    Typo error fixed

--- src/client/mappingparty.js
+++ src/client/mappingparty.js
@@ -2,6 +2,6 @@ Meteor.subscribe('users');
 Meteor.subscribe('tags');
 Meteor.subscribe('elements');
 
-Users = new Meteror.Collection('users');
-Tags = new Meteror.Collection('tags');
-Elements = new Meteror.Collection('elements');
+Users = new Meteor.Collection('users');
+Tags = new Meteor.Collection('tags');
+Elements = new Meteor.Collection('elements');
